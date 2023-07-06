effea6a62039ebd068a58cbe991cfe50940fb83e
seaurchin-miau <annaluzgp@gmail.com>
2023-07-06 11:32:16 -0600

    "Controller,repository y service de Usuario"

---
+++ src/main/java/com/curso/ecommerce/service/UsuarioServiceImpl.java
@@ -0,0 +1,36 @@
+package com.curso.ecommerce.service;
+
+import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.repository.IUsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioServiceImpl implements IUsuarioService {
+
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+
+    @Override
+    public Optional<Usuario> findById(Integer id) {
+        return usuarioRepository.findById(id);
+    }
+
+    @Override
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public Optional<Usuario> findByEmail(String email) {
+        return usuarioRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+}
