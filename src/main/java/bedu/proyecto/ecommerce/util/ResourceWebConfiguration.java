6bf5ee84904c74bc7f9ea5e406715b78c8d937d5
eliasfire <vakito85@gmail.com>
2023-07-10 22:30:51 -0300

    cambios en SpringBootSecurity

--- src/main/java/bedu/proyecto/ecommerce/util/ResourceWebConfiguration.java
+++ src/main/java/bedu/proyecto/ecommerce/util/ResourceWebConfiguration.java
@@ -1,4 +1,4 @@
-package bedu.proyecto.ecommerce;
+package bedu.proyecto.ecommerce.util;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
---
+++ src/main/java/bedu/proyecto/ecommerce/util/SecurityConfig.java
@@ -0,0 +1,43 @@
+package bedu.proyecto.ecommerce.util;
+
+import bedu.proyecto.ecommerce.service.UserDetailServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final UserDetailServiceImpl userDetailService;
+
+    public SecurityConfig(UserDetailServiceImpl userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().authorizeHttpRequests().requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/user/**").hasRole("USER").anyRequest().permitAll()
+                .and().formLogin().loginPage("/login").  defaultSuccessUrl("/login/access").and().logout().logoutSuccessUrl("/close");
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+}
+
